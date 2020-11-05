f45123d619d2bc73a8381f21b597ecc5e4aeab5b
whuang8 <mail.william.huang@gmail.com>
2020-11-04 19:16:19 -0600

    Use google vision to detect 🌭!🌭

--- app/controllers/hotdog_controller.rb
+++ app/controllers/hotdog_controller.rb
@@ -1,9 +1,27 @@
+# frozen_string_literal: true
+
+require 'google/cloud/vision'
+
 class HotdogController < ApplicationController
-  def index
-  end
+  def index; end
 
   def upload
+    image_annotator = Google::Cloud::Vision.image_annotator
 
-    redirect_to root_path
+    # The name of the image file to annotate
+    file_name = params[:picture].path
+
+    # Performs label detection on the image file
+    response = image_annotator.label_detection image: file_name
+    response.responses.each do |res|
+      puts 'Labels:'
+      res.label_annotations.each do |label|
+        puts label.description
+        @result = 'Hotdog!' if label.description.downcase == 'hot dog'
+      end
+    end
+
+    @result ||= 'Not hotdog!'
+    render 'index'
   end
 end
